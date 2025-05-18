abd99bf3b12b8952e748c0894aa74d8065281cda
Urbán Ákos <h378903@stud.u-szeged.hu>
2025-05-18 09:43:46 +0200

    régi 5-5 beszúrás kiszedése

--- sql/02_adatfeltoltes.sql
+++ sql/02_adatfeltoltes.sql
@@ -1,39 +1,5 @@
 
 -- 1. Adatok beszúrása: Felhasznalo
-INSERT INTO Felhasznalo (
-	felhasznalo_id, felhasznalonev, email, jelszo, szerepkor, 
-	regisztracio_idopont, utolso_bejelentkezes, profilkep_url, bio)
-VALUES (
-	felhasznalo_seq.NEXTVAL, 'Munkácsy Mihály', 'munkacsy.mihaly@pelda.com', 'A_zeneszoba1878', 'user', 
-	TIMESTAMP '2022-01-15 10:30:00', TIMESTAMP '2023-05-20 14:45:00', 'https://pelda.com/profilkepek/munkacsy.jpg', '...');
-
-INSERT INTO Felhasznalo (
-	felhasznalo_id, felhasznalonev, email, jelszo, szerepkor, 
-	regisztracio_idopont, utolso_bejelentkezes, profilkep_url, bio)
-VALUES (
-	felhasznalo_seq.NEXTVAL, 'Liszt Ferenc', 'liszt.ferenc@pelda.com', 'Magyar_rapszodiak1853', 'admin', 
-	TIMESTAMP '2022-02-20 09:15:00', TIMESTAMP '2023-05-19 16:30:00', 'https://pelda.com/profilkepek/lisztferenc.jpg', 'Minden művészet egyazon forrásból ered.');
-
-INSERT INTO Felhasznalo (
-	felhasznalo_id, felhasznalonev, email, jelszo, szerepkor, 
-	regisztracio_idopont, utolso_bejelentkezes, profilkep_url, bio)
-VALUES (
-	felhasznalo_seq.NEXTVAL, 'Robert Capa', 'robert.capa@pelda.com', 'Kisse_elmosodva1947', 'user', 
-	TIMESTAMP '2022-03-10 14:20:00', TIMESTAMP '2023-05-18 11:25:00', 'https://pelda.com/profilkepek/robertcapa.jpg', 'Ha nem elég jók a képeid, nem voltál elég közel.');
-
-INSERT INTO Felhasznalo (
-	felhasznalo_id, felhasznalonev, email, jelszo, szerepkor, 
-	regisztracio_idopont, utolso_bejelentkezes, profilkep_url, bio)
-VALUES (
-	felhasznalo_seq.NEXTVAL, 'Jávor Pál', 'javor.pal@pelda.com', 'Halalos_tavasz1939', 'user', 
-	TIMESTAMP '2022-04-05 11:45:00', TIMESTAMP '2023-05-17 19:10:00', 'https://pelda.com/profilkepek/javorpal.jpg', 'Csak azt is eljátszottam, ha kellett.');
-
-INSERT INTO Felhasznalo (
-	felhasznalo_id, felhasznalonev, email, jelszo, szerepkor, 
-	regisztracio_idopont, utolso_bejelentkezes, profilkep_url, bio)
-VALUES (
-	felhasznalo_seq.NEXTVAL, 'Babits Mihály', 'babits.mihaly@pelda.com', 'A_golyakalifa1913', 'user', 
-	TIMESTAMP '2022-05-12 16:50:00', TIMESTAMP '2023-05-16 08:55:00', 'https://pelda.com/profilkepek/babitsmihaly.jpg', 'A kimerült író olyan, mint a megcsalt férj: maga veszi észre legkésőbb a szomorú igazságot.');
 
 INSERT INTO Felhasznalo (
     felhasznalo_id, felhasznalonev, email, jelszo, szerepkor,
@@ -1237,12 +1203,6 @@ INSERT INTO Felhasznalo (
 
 
 -- 2. Adatok beszúrása: Kategoria
-INSERT INTO Kategoria ( nev, leiras) VALUES (kategoria_seq.NEXTVAL, 'Festészet', 'Videók festőkről, festészetről');
-INSERT INTO Kategoria (kategoria_id, nev, leiras) VALUES (kategoria_seq.NEXTVAL, 'Zeneművészet', 'Koncertfelvételek, zeneművek videói, okatatóanyagok');
-INSERT INTO Kategoria (kategoria_id, nev, leiras) VALUES (kategoria_seq.NEXTVAL, 'Fotóművészet', 'Fotózással és fotósokkal kapcsolatos videók');
-INSERT INTO Kategoria (kategoria_id, nev, leiras) VALUES (kategoria_seq.NEXTVAL, 'Színészet', 'Színészekkel és színészettel foglalkozó videók');
-INSERT INTO Kategoria (kategoria_id, nev, leiras) VALUES (kategoria_seq.NEXTVAL, 'Irodalom-művészet', 'Irodalommal, foglalkozó videók, oktatóanyagok');
-
 INSERT INTO Kategoria (kategoria_id, nev, leiras) VALUES (kategoria_seq.NEXTVAL, 'Kategória 1', 'Ez a(z) 1. kategória leírása');
 INSERT INTO Kategoria (kategoria_id, nev, leiras) VALUES (kategoria_seq.NEXTVAL, 'Kategória 2', 'Ez a(z) 2. kategória leírása');
 INSERT INTO Kategoria (kategoria_id, nev, leiras) VALUES (kategoria_seq.NEXTVAL, 'Kategória 3', 'Ez a(z) 3. kategória leírása');
@@ -1296,23 +1256,6 @@ INSERT INTO Kategoria (kategoria_id, nev, leiras) VALUES (kategoria_seq.NEXTVAL,
 
 
 -- 3. Adatok beszúrása: Video
-INSERT INTO Video (video_id, cim, leiras, feltoltes_datum, felhasznalo_id, hossz, is_short, video_url)
-VALUES (video_seq.NEXTVAL, 'Bevezetés a festészetbe', 'A színek pszichológiai hatása, színkeverés', TIMESTAMP '2023-02-10 09:30:00', 1, 3, 845, 0, 'https://pelda.com/videok/szinkeveres.mp4');
-
-INSERT INTO Video (video_id, cim, leiras, feltoltes_datum, felhasznalo_id, hossz, is_short, video_url)
-VALUES (video_seq.NEXTVAL, 'Liszt Ferenc: II. Magyar rapszódia', 'November 17-én a hazai művészeti élet 69 kiválósága vette át a Nemzet Művésze díjat a Vigadóban. Az ünnepi rendezvényen Liszt Ferenc II. Magyar rapszódiáját Bogányi Gergely Kossuth- és Liszt Ferenc-díjas zongoraművész előadásában hallgathatták meg a díjazottak.', TIMESTAMP '2023-03-25 14:15:00', 2, 4, 1256, 0, 'https://pelda.com/videok/magyar-rapszodia.mp4');
-
-INSERT INTO Video (video_id, cim, leiras, feltoltes_datum, felhasznalo_id, hossz, is_short, video_url)
-VALUES (video_seq.NEXTVAL, 'Hogyan készíts jó fotókat a nyaraláson', 'Tanuld meg, a fotózás fortályait, hogy a barátaid számára is élvezetesek legyenek a fotóid!', TIMESTAMP '2023-05-05 11:20:00', 3, 5, 732, 0, 'https://pelda.com/videok/fotozas.mp4');
-
-INSERT INTO Video (video_id, cim, leiras, feltoltes_datum, felhasznalo_id, hossz, is_short, video_url)
-VALUES (video_seq.NEXTVAL, 'Csehov - Cseresznyéskert', '2007. március 17-i bemutató felvétele a Vígszínházból.', TIMESTAMP '2023-02-15 16:45:00', 4, 3, 3650, 0, 'https://pelda.com/videok/cseresznyeskert.mp4');
-
-INSERT INTO Video (video_id, cim, leiras, feltoltes_datum, felhasznalo_id, hossz, is_short, video_url)
-VALUES (video_seq.NEXTVAL, 'Magyar irodalom-érettségi témakörök', 'Átbeszáljük az érettségi témaköröket, hogy ne érjen meglepetés az érettségin!', TIMESTAMP '2023-01-30 10:00:00', 5, 1, 612, 1, 'https://pelda.com/videok/irodalom-erettsegi.mp4');
-
-
-
 INSERT INTO Video (
     video_id, cim, leiras, feltoltes_datum, felhasznalo_id, hossz, is_short, video_url
 ) VALUES (
@@ -2216,22 +2159,6 @@ INSERT INTO Video (
 
 -- 4. Adatok beszúrása: VideoMetadata
 INSERT INTO VideoMetadata (video_url, miniatur_url, beagyazasi_kod)
-VALUES ('https://pelda.com/videok/szinkeveres.mp4', 'https://pelda.com/miniaturok/szinkeveres.jpg', '<iframe src="https://pelda.com/beagyazas/szinkeveres"></iframe>');
-
-INSERT INTO VideoMetadata (video_url, miniatur_url, beagyazasi_kod)
-VALUES ('https://pelda.com/videok/magyar-rapszodia.mp4', 'https://pelda.com/miniaturok/magyar-rapszodia.jpg', '<iframe src="https://pelda.com/beagyazas/magyar-rapszodia"></iframe>');
-
-INSERT INTO VideoMetadata (video_url, miniatur_url, beagyazasi_kod)
-VALUES ('https://pelda.com/videok/fotozas.mp4', 'https://pelda.com/miniaturok/fotozas.jpg', '<iframe src="https://pelda.com/beagyazas/fotozas"></iframe>');
-
-INSERT INTO VideoMetadata (video_url, miniatur_url, beagyazasi_kod)
-VALUES ('https://pelda.com/videok/cseresznyeskert.mp4', 'https://pelda.com/miniaturok/cseresznyeskert.jpg', '<iframe src="https://pelda.com/beagyazas/cseresznyeskert"></iframe>');
-
-INSERT INTO VideoMetadata (video_url, miniatur_url, beagyazasi_kod)
-VALUES ('https://pelda.com/videok/irodalom-erettsegi.mp4', 'https://pelda.com/miniaturok/irodalom-erettsegi.jpg', '<iframe src="https://pelda.com/beagyazas/irodalom-erettsegi"></iframe>');
-
-
-INSERT INTO VideoMetadata (video_url, miniatur_url, beagyazasi_kod)
 VALUES ('https://video.hu/video_1.mp4', 'https://video.hu/thumb_1.jpg', '<iframe src="https://video.hu/video_1.mp4"></iframe>');
 
 
@@ -2834,21 +2761,6 @@ VALUES ('https://video.hu/video_150.mp4', 'https://video.hu/thumb_150.jpg', '<if
 
 
 -- 5. Adatok beszúrása: Hozzaszolas
-INSERT INTO Hozzaszolas (hozzaszolas_id, felhasznalo_id, video_id, hozzaszolas_szoveg, letrehozas_datum)
-VALUES (hozzaszolas_seq.NEXTVAL, 2, 1, 'Nagyon sokat tanultam a videódból! Lesz videó az ecsethasználatról valamikor?', TIMESTAMP '2023-04-10 14:25:00');
-
-INSERT INTO Hozzaszolas (hozzaszolas_id, felhasznalo_id, video_id, hozzaszolas_szoveg, letrehozas_datum)
-VALUES (hozzaszolas_seq.NEXTVAL, 2, 2, 'Köszi a feltöltést! Sajnos nem lehettem ott az előadáson, de nagyon örülök, hogy meghallgathattam felvételről.', TIMESTAMP '2023-03-26 09:30:00');
-
-INSERT INTO Hozzaszolas (hozzaszolas_id, felhasznalo_id, video_id, hozzaszolas_szoveg, letrehozas_datum)
-VALUES (hozzaszolas_seq.NEXTVAL, 3, 3, 'Kipróbáltam ezeket a tippeket és nagyon látványos fotókat készítettem a családi nyaraláson! Lesz mit a fotóalbumba tenni!', TIMESTAMP '2023-05-06 18:15:00');
-
-INSERT INTO Hozzaszolas (hozzaszolas_id, felhasznalo_id, video_id, hozzaszolas_szoveg, letrehozas_datum)
-VALUES (hozzaszolas_seq.NEXTVAL, 5, 4, 'Csodálatos darab és nagyon jó felvétel!', TIMESTAMP '2023-02-16 20:45:00');
-
-INSERT INTO Hozzaszolas (hozzaszolas_id, felhasznalo_id, video_id, hozzaszolas_szoveg, letrehozas_datum)
-VALUES (hozzaszolas_seq.NEXTVAL, 4, 5, 'Én tavaly érettségiztem és nagyon sokat segített ez a videó a felkészülésben.', TIMESTAMP '2023-01-31 11:10:00');
-
 INSERT INTO Hozzaszolas (
     hozzaszolas_id, felhasznalo_id, video_id, hozzaszolas_szoveg, letrehozas_datum
 ) VALUES (
@@ -3451,12 +3363,6 @@ INSERT INTO Hozzaszolas (
 
 
 -- 6. Adatok beszúrása: Cimke
-INSERT INTO Cimke (cimke_id, nev) VALUES (cimke_seq.NEXTVAL, 'festészet');
-INSERT INTO Cimke (cimke_id, nev) VALUES (cimke_seq.NEXTVAL, 'zene');
-INSERT INTO Cimke (cimke_id, nev) VALUES (cimke_seq.NEXTVAL, 'fotózás');
-INSERT INTO Cimke (cimke_id, nev) VALUES (cimke_seq.NEXTVAL, 'színház');
-INSERT INTO Cimke (cimke_id, nev) VALUES (cimke_seq.NEXTVAL, 'oktatás');
-
 INSERT INTO Cimke (cimke_id, nev) VALUES (cimke_seq.NEXTVAL, 'Címke 1');
 INSERT INTO Cimke (cimke_id, nev) VALUES (cimke_seq.NEXTVAL, 'Címke 2');
 INSERT INTO Cimke (cimke_id, nev) VALUES (cimke_seq.NEXTVAL, 'Címke 3');
@@ -3509,11 +3415,6 @@ INSERT INTO Cimke (cimke_id, nev) VALUES (cimke_seq.NEXTVAL, 'Címke 49');
 INSERT INTO Cimke (cimke_id, nev) VALUES (cimke_seq.NEXTVAL, 'Címke 50');
 
 -- 7. Adatok beszúrása: VideoCimke
-INSERT INTO VideoCimke (video_id, cimke_id) VALUES (1, 1);
-INSERT INTO VideoCimke (video_id, cimke_id) VALUES (2, 2);
-INSERT INTO VideoCimke (video_id, cimke_id) VALUES (3, 3);
-INSERT INTO VideoCimke (video_id, cimke_id) VALUES (4, 4);
-INSERT INTO VideoCimke (video_id, cimke_id) VALUES (5, 5);
 INSERT INTO VideoCimke (video_id, cimke_id) VALUES (1, 2);
 INSERT INTO VideoCimke (video_id, cimke_id) VALUES (1, 3);
 INSERT INTO VideoCimke (video_id, cimke_id) VALUES (2, 3);
@@ -3817,20 +3718,6 @@ INSERT INTO VideoCimke (video_id, cimke_id) VALUES (150, 1);
 
 
 -- 8. Adatok beszúrása: LejatszasiLista
-INSERT INTO LejatszasiLista (lista_id, nev, felhasznalo_id, publikus, letrehozas_datum)
-VALUES (lejatszasilista_seq.NEXTVAL, 'Tutoriálok', 1, 1, TIMESTAMP '2023-01-20 15:30:00');
-
-INSERT INTO LejatszasiLista (lista_id, nev, felhasznalo_id, publikus, letrehozas_datum)
-VALUES (lejatszasilista_seq.NEXTVAL, 'Élő zene', 4, 1, TIMESTAMP '2023-02-10 12:45:00');
-
-INSERT INTO LejatszasiLista (lista_id, nev, felhasznalo_id, publikus, letrehozas_datum)
-VALUES (lejatszasilista_seq.NEXTVAL, 'Fotózás', 5, 1, TIMESTAMP '2023-03-05 09:20:00');
-
-INSERT INTO LejatszasiLista (lista_id, nev, felhasznalo_id, publikus, letrehozas_datum)
-VALUES (lejatszasilista_seq.NEXTVAL, 'Színdarabok', 3, 1, TIMESTAMP '2023-04-15 18:10:00');
-
-INSERT INTO LejatszasiLista (lista_id, nev, felhasznalo_id, publikus, letrehozas_datum)
-VALUES (lejatszasilista_seq.NEXTVAL, 'Érettségi felkészülés - irodalom', 2, 0, TIMESTAMP '2023-05-01 14:00:00');
 
 INSERT INTO LejatszasiLista (
     lista_id, nev, felhasznalo_id, publikus, letrehozas_datum
@@ -3841,25 +3728,6 @@ INSERT INTO LejatszasiLista (
 
 
 -- 9. Adatok beszúrása: LejatszasiListaVideo
-INSERT INTO LejatszasiListaVideo (lista_id, video_id, pozicio, hozzaadas_datum)
-VALUES (1, 1, 1, TIMESTAMP '2023-04-11 10:15:00');
-
-INSERT INTO LejatszasiListaVideo (lista_id, video_id, pozicio, hozzaadas_datum)
-VALUES (2, 2, 1, TIMESTAMP '2023-03-27 16:30:00');
-
-INSERT INTO LejatszasiListaVideo (lista_id, video_id, pozicio, hozzaadas_datum)
-VALUES (3, 3, 1, TIMESTAMP '2023-05-07 11:45:00');
-
-INSERT INTO LejatszasiListaVideo (lista_id, video_id, pozicio, hozzaadas_datum)
-VALUES (4, 4, 1, TIMESTAMP '2023-02-17 19:20:00');
-
-INSERT INTO LejatszasiListaVideo (lista_id, video_id, pozicio, hozzaadas_datum)
-VALUES (5, 5, 1, TIMESTAMP '2023-02-01 13:10:00');
-
-INSERT INTO LejatszasiListaVideo (lista_id, video_id, pozicio, hozzaadas_datum) VALUES (1, 2, 1, SYSTIMESTAMP);
-INSERT INTO LejatszasiListaVideo (lista_id, video_id, pozicio, hozzaadas_datum) VALUES (1, 3, 2, SYSTIMESTAMP);
-INSERT INTO LejatszasiListaVideo (lista_id, video_id, pozicio, hozzaadas_datum) VALUES (1, 4, 3, SYSTIMESTAMP);
-
 INSERT INTO LejatszasiLista (
     lista_id, nev, felhasznalo_id, publikus, letrehozas_datum
 ) VALUES (
@@ -4032,12 +3900,6 @@ INSERT INTO LejatszasiListaVideo (lista_id, video_id, pozicio, hozzaadas_datum) 
 INSERT INTO LejatszasiListaVideo (lista_id, video_id, pozicio, hozzaadas_datum) VALUES (20, 61, 3, SYSTIMESTAMP);
 
 -- 10. Adatok beszúrása: Kedvencek
-INSERT INTO Kedvencek (felhasznalo_id, video_id, hozzaadas_datum) VALUES (1, 3, TIMESTAMP '2023-05-08 09:45:00');
-INSERT INTO Kedvencek (felhasznalo_id, video_id, hozzaadas_datum) VALUES (2, 5, TIMESTAMP '2023-02-02 14:30:00');
-INSERT INTO Kedvencek (felhasznalo_id, video_id, hozzaadas_datum) VALUES (3, 2, TIMESTAMP '2023-03-28 17:15:00');
-INSERT INTO Kedvencek (felhasznalo_id, video_id, hozzaadas_datum) VALUES (4, 1, TIMESTAMP '2023-04-12 11:20:00');
-INSERT INTO Kedvencek (felhasznalo_id, video_id, hozzaadas_datum) VALUES (5, 4, TIMESTAMP '2023-02-18 20:05:00');
-
 INSERT INTO Kedvencek (felhasznalo_id, video_id, hozzaadas_datum) VALUES (2, 4, SYSTIMESTAMP);
 INSERT INTO Kedvencek (felhasznalo_id, video_id, hozzaadas_datum) VALUES (3, 7, SYSTIMESTAMP);
 INSERT INTO Kedvencek (felhasznalo_id, video_id, hozzaadas_datum) VALUES (4, 10, SYSTIMESTAMP);
@@ -4141,21 +4003,6 @@ INSERT INTO Kedvencek (felhasznalo_id, video_id, hozzaadas_datum) VALUES (101, 1
 
 
 -- 11. Adatok beszúrása: Megtekintes
-INSERT INTO Megtekintes (megtekintes_id, video_id, felhasznalo_id, megtekintes_datum, megtekintes_hossz)
-VALUES (megtekintes_seq.NEXTVAL, 1, 2, TIMESTAMP '2023-04-11 13:10:00', 845);
-
-INSERT INTO Megtekintes (megtekintes_id, video_id, felhasznalo_id, megtekintes_datum, megtekintes_hossz)
-VALUES (megtekintes_seq.NEXTVAL, 2, 1, TIMESTAMP '2023-03-26 10:25:00', 1200);
-
-INSERT INTO Megtekintes (megtekintes_id, video_id, felhasznalo_id, megtekintes_datum, megtekintes_hossz)
-VALUES (megtekintes_seq.NEXTVAL, 3, 3, TIMESTAMP '2023-05-06 19:30:00', 732);
-
-INSERT INTO Megtekintes (megtekintes_id, video_id, felhasznalo_id, megtekintes_datum, megtekintes_hossz)
-VALUES (megtekintes_seq.NEXTVAL, 4, 5, TIMESTAMP '2023-02-16 21:15:00', 3000);
-
-INSERT INTO Megtekintes (megtekintes_id, video_id, felhasznalo_id, megtekintes_datum, megtekintes_hossz)
-VALUES (megtekintes_seq.NEXTVAL, 5, 4, TIMESTAMP '2023-01-31 12:40:00', 612);
-
 INSERT INTO Megtekintes (
     megtekintes_id, video_id, felhasznalo_id, megtekintes_datum, megtekintes_hossz
 ) VALUES (
@@ -4764,22 +4611,6 @@ INSERT INTO Megtekintes (
 );
 
 -- 12. Adatok beszúrása: Ajanlasok
-INSERT INTO Ajanlasok (ajanlas_id, felhasznalo_id, video_id, ajanlas_erosseg, ajanlas_datum)
-VALUES (ajanlas_seq.NEXTVAL, 1, 4, 0.85, TIMESTAMP '2023-05-20 15:00:00');
-
-INSERT INTO Ajanlasok (ajanlas_id, felhasznalo_id, video_id, ajanlas_erosseg, ajanlas_datum)
-VALUES (ajanlas_seq.NEXTVAL, 2, 2, 0.78, TIMESTAMP '2023-05-19 17:00:00');
-
-INSERT INTO Ajanlasok (ajanlas_id, felhasznalo_id, video_id, ajanlas_erosseg, ajanlas_datum)
-VALUES (ajanlas_seq.NEXTVAL, 3, 5, 0.92, TIMESTAMP '2023-05-18 12:00:00');
-
-INSERT INTO Ajanlasok (ajanlas_id, felhasznalo_id, video_id, ajanlas_erosseg, ajanlas_datum)
-VALUES (ajanlas_seq.NEXTVAL, 4, 3, 0.65, TIMESTAMP '2023-05-17 20:00:00');
-
-INSERT INTO Ajanlasok (ajanlas_id, felhasznalo_id, video_id, ajanlas_erosseg, ajanlas_datum)
-VALUES (ajanlas_seq.NEXTVAL, 5, 1, 0.73, TIMESTAMP '2023-05-16 09:30:00');
-
-
 INSERT INTO Ajanlasok (
     ajanlas_id, felhasznalo_id, video_id, ajanlas_erosseg, ajanlas_datum
 ) VALUES (
